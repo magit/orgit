4a9c4f770a118348bbf6fb6ee4a7629275b8a0d2
Jonas Bernoulli <jonas@bernoul.li>
2023-01-20 22:44:16 +0100

    Delay loading until both Org and Magit have been loaded

--- orgit.el
+++ orgit.el
@@ -255,11 +255,12 @@ The second pass accepts these specs:
 
 ;;;###autoload
 (with-eval-after-load 'org
-  (org-link-set-parameters "orgit"
-                           :store    #'orgit-status-store
-                           :follow   #'orgit-status-open
-                           :export   #'orgit-status-export
-                           :complete #'orgit-status-complete-link))
+  (with-eval-after-load 'magit
+    (org-link-set-parameters "orgit"
+                             :store    #'orgit-status-store
+                             :follow   #'orgit-status-open
+                             :export   #'orgit-status-export
+                             :complete #'orgit-status-complete-link)))
 
 ;;;###autoload
 (defun orgit-status-store ()
@@ -291,11 +292,12 @@ In that case `orgit-rev-store' stores one or more links instead."
 
 ;;;###autoload
 (with-eval-after-load 'org
-  (org-link-set-parameters "orgit-log"
-                           :store    #'orgit-log-store
-                           :follow   #'orgit-log-open
-                           :export   #'orgit-log-export
-                           :complete #'orgit-log-complete-link))
+  (with-eval-after-load 'magit
+    (org-link-set-parameters "orgit-log"
+                             :store    #'orgit-log-store
+                             :follow   #'orgit-log-open
+                             :export   #'orgit-log-export
+                             :complete #'orgit-log-complete-link)))
 
 ;;;###autoload
 (defun orgit-log-store ()
@@ -355,11 +357,12 @@ In that case `orgit-rev-store' stores one or more links instead."
 
 ;;;###autoload
 (with-eval-after-load 'org
-  (org-link-set-parameters "orgit-rev"
-                           :store    #'orgit-rev-store
-                           :follow   #'orgit-rev-open
-                           :export   #'orgit-rev-export
-                           :complete #'orgit-rev-complete-link))
+  (with-eval-after-load 'magit
+    (org-link-set-parameters "orgit-rev"
+                             :store    #'orgit-rev-store
+                             :follow   #'orgit-rev-open
+                             :export   #'orgit-rev-export
+                             :complete #'orgit-rev-complete-link)))
 
 ;;;###autoload
 (defun orgit-rev-store ()

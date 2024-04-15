ca3bf7aa20074e25fee37b71cfef127b93a3dde7
Jonas Bernoulli <jonas@bernoul.li>
2024-04-15 17:46:28 +0200

    Update contact information

--- orgit.el
+++ orgit.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2014-2024 The Magit Project Contributors
 
-;; Author: Jonas Bernoulli <jonas@bernoul.li>
+;; Author: Jonas Bernoulli <emacs.orgit@jonas.bernoulli.dev>
 ;; Homepage: https://github.com/magit/orgit
 ;; Keywords: hypermedia vc
 

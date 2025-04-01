efd98e5caaac1d08677dae95be40fab65dcda2c8
Jonas Bernoulli <jonas@bernoul.li>
2025-04-01 20:10:22 +0200

    Release version 2.0.2

--- orgit.el
+++ orgit.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/magit/orgit
 ;; Keywords: hypermedia vc
 
-;; Package-Version: 2.0.1
+;; Package-Version: 2.0.2
 ;; Package-Requires: (
 ;;     (emacs "27.1")
 ;;     (compat "30.0.2.0")
